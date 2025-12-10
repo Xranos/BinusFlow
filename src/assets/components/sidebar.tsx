292c2e1eec678ee3429c9127773f6fa877a2ce97
Xranos <ryanfrederick02@gmail.com>
2025-12-10 16:29:56 +0700

    sidebar on progress

--- src/assets/components/sidebar.tsx
+++ src/assets/components/sidebar.tsx
@@ -1,4 +1,7 @@
-import { Link, useLocation} from "react-router-dom";
+import { Link } from "react-router-dom";
+
+import DashboardPage from "../../pages/dashboardPage";
+import ConfigPage from "../../pages/configPage";
 
 import { FaStickyNote } from "react-icons/fa";
 import { IoMdHome } from "react-icons/io";
@@ -10,21 +13,41 @@ function Sidebar() {
     // const location = useLocation();
     // const [isExpanded, setIsExpanded] = useState(true);
     return (
-        <aside className="h-screen">
-            <nav className ="h-full flex flex-col bg-gray-800">
-                <div className="flex justify-between items-center p-4">
-                    <div className="flex items-center gap-2 ">
-                    <h1 className="text-red-500 text-2xl p-2">BinusFlow</h1>
+        <aside className="h-screen w-64 p-2 ">
+            <nav className="h-full flex flex-col bg-gray-800 border rounded-md">
+                <div className="justify-between items-center p-4">
+                    <div className="items-center gap-2 ">
+                        <h1 className="text-red-500 text-3xl p-4 gap-2 flex">
+                            <FaStickyNote /> BinusFlow</h1>
+                    </div>
+                    <div className="flex flex-col justify-between items-left gap-5">
+                        <span className="flex text-2xl text-white gap-2 m-2">
+                            <IoIosSettings className="text-4xl" />
+                            <div className="hover:bg-gray-700 rounded-md p-1 w-full">
+                                <Link to="/">
+                                    <h2>
+                                        Home
+                                    </h2>
+                                </Link>
+                            </div>
+                        </span>
+                        <span className="flex text-2xl text-white gap-2 m-2">
+                            <IoMdHome className="text-4xl" />
+                            <span className="hover:bg-gray-700 rounded-md p-1">
+                                <Link to="/Config">
+                                    <h2>
+                                        Configuration
+                                    </h2>
+                                </Link>
+                            </span>
+                        </span>
 
                     </div>
-                    
+
 
                 </div>
-                
+
             </nav>
-    
-
-
         </aside>
     )
 }
